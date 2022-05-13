8987bb849ab448a5e09940d6e040e37636ccab55
Stanley <1jony2jony@gmail.com>
2022-05-13 10:53:20 -0300

    Aulas php-mysql 2 a 6

---
+++ curso-php/php-mysql/4_criar_deletar_tabelas/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "cursophp";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+//$q = "CREATE TABLE teste (nome VARCHAR(100), sobrenome VARCHAR(100))";
+$q = "DROP TABLE teste";
+
+$conn->query($q);
+
+$conn->close();
