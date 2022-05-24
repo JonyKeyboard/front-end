daef7186ab63c44e4e0a2a3c5e19ab0dbd8262c3
Stanley <1jony2jony@gmail.com>
2022-05-24 09:21:17 -0300

    Add Registro de contatos

--- curso-php/agenda/config/process.php
+++ curso-php/agenda/config/process.php
@@ -5,12 +5,36 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
-$contacts = [];
+$id;
 
-$query = "SELECT * FROM contacts";
+if(!empty($_GET)) {
+    $id = $_GET["id"];
+}
 
-$stmt = $conn->prepare($query);
+// um único contato
+if(!empty($id)) {
 
-$stmt->execute();
+    $query = "SELECT * FROM contacts WHERE id = :id";
 
-$contacts = $stmt->fetchAll();
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam(":id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+
+} else {
+    // todos os contatos
+    $contacts = [];
+
+    $query = "SELECT * FROM contacts";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->execute();
+
+    $contacts = $stmt->fetchAll();
+
+}
+
