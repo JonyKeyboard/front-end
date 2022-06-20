cd255960c6c4ebbcd9696eddebbeecc4e57f12d6
Stanley <1jony2jony@gmail.com>
2022-06-20 09:38:35 -0300

    Finalizando model Movie e css newmovie

--- curso-php/moviestar/models/Movie.php
+++ curso-php/moviestar/models/Movie.php
@@ -11,12 +11,24 @@
         public $lenght;
         public $user_id;
 
+        
+    public function imageGenerateName() {
+        return bin2hex(random_bytes(60)) . ".jpg";
     }
+}
+
 
     interface MovieDAOInterface {
 
         public function buildMovie($data);
+        public function findAll();
+        public function getLatestMovies();
+        public function getMoviesByCategory($category);
+        public function getMoviesByUserId($id);
+        public function findById($id);
+        public function findByTitle($title);
         public function create(Movie $movie);
         public function update(Movie $movie);
+        public function destroy($id);
 
     }
